90cde8d85dc69eceb46cb2e4d39185b7c4c9a093
Igor Santos <igorsantoscosta@gmail.com>
2023-01-26 23:24:01 -0300

    Edit logo navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
+import useEvent from "@react-hook/event";
 import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { List, X } from "phosphor-react";
 import { ReactNode, useEffect, useState } from "react";
+import logoWhiteImg from "../assets/logo-branca.png";
 import logoImg from "../assets/logo.png";
 
 interface NavbarComponentProps {
@@ -13,63 +15,81 @@ interface NavbarComponentProps {
 const NavbarComponent = ({ children }: NavbarComponentProps) => {
   const router = useRouter();
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const [isOpenMenuMobile, setIsOpenMenuMobile] = useState(false);
+  const [isOpenBackdrop, setIsOpenBackdrop] = useState(false);
 
   useEffect(() => {
-    setIsOpen(false);
+    setIsOpenMenuMobile(false);
   }, [router?.pathname]);
 
   useEffect(() => {
-    document.getElementsByTagName("html")[0].style.overflowY = !isOpen
+    document.getElementsByTagName("html")[0].style.overflowY = !isOpenMenuMobile
       ? "visible"
       : "hidden";
-  }, [isOpen]);
+
+    setTimeout(() => setIsOpenBackdrop(isOpenMenuMobile), 50);
+  }, [isOpenMenuMobile]);
+
+  const handleWindowResize = () => {
+    const windowWidth = document.querySelector("html")?.clientWidth || 0;
+
+    if (windowWidth < 768 && !isMobile) {
+      setIsMobile(true);
+    }
+    if (windowWidth > 768 && isMobile) {
+      setIsMobile(false);
+    }
+  };
+
+  useEvent(window, "resize", (event) => handleWindowResize());
 
   return (
     <>
-      <div>
-        <div className="fixed z-[99998] top-0 inset-x-0 md:relative bg-primary md:bg-transparent flex flex-row items-center justify-between py-2 px-5 md:px-0 md:py-14">
-          <Image
-            src={logoImg}
-            alt="Logo from Rodap"
-            priority
-            className="h-7 md:h-10 w-auto"
-          />
-          <ul className="flex-row gap-4 items-center justify-center hidden md:flex">
-            {children}
-          </ul>
-          <List
+      <div className="fixed z-[99998] top-0 inset-x-0 md:static bg-primary border-b-2 border-white md:border-0 md:bg-transparent flex flex-row items-center justify-between py-3 px-5 md:px-0 md:py-14">
+        <Image
+          src={isMobile ? logoWhiteImg : logoImg}
+          alt="Logo from Rodap"
+          priority
+          className="h-7 md:h-10 w-auto"
+        />
+        <ul className="flex-row gap-4 items-center justify-center hidden md:flex">
+          {children}
+        </ul>
+        <List
+          size={32}
+          color="#ffffff"
+          className="cursor-pointer md:hidden"
+          onClick={() => setIsOpenMenuMobile(true)}
+        />
+        <div
+          className={clsx(
+            "absolute md:hidden h-screen top-0 z-[99999] text-white bg-primary transition-all duration-500 px-4 pt-4 w-[220px] max-w-full",
+            isOpenMenuMobile ? "left-0" : "-left-full"
+          )}
+        >
+          <X
             size={32}
             color="#ffffff"
-            className="cursor-pointer"
-            onClick={() => setIsOpen(true)}
+            className="ml-auto cursor-pointer"
+            onClick={() => setIsOpenMenuMobile(false)}
           />
+          <ul className="flex flex-col gap-4 items-start">{children}</ul>
+        </div>
+        {isOpenMenuMobile && (
           <div
             className={clsx(
-              "absolute md:hidden h-screen top-0 z-[99999] text-white bg-primary transition-all duration-200 px-4 pt-4 w-[220px] max-w-full",
-              isOpen ? "left-0" : "-left-full"
-            )}
-          >
-            <X
-              size={32}
-              color="#ffffff"
-              className="ml-auto"
-              onClick={() => setIsOpen(false)}
-            />
-            <ul className="flex flex-col gap-4 items-start">{children}</ul>
-          </div>
-          <div
-            className={clsx(
-              "absolute inset-x-0 top-0 h-screen transition-all duration-500 block md:hidden",
-              isOpen
-                ? "z-[99997] bg-black/50 opacity-100 delay-100"
+              "absolute inset-x-0 top-0 h-screen transition-all duration-300 block md:hidden",
+              isOpenBackdrop
+                ? "z-[99997] bg-black/50 opacity-100"
                 : "bg-transparent opacity-0 -z-50"
             )}
-            onClick={() => setIsOpen(false)}
+            onClick={() => setIsOpenMenuMobile(false)}
           ></div>
-        </div>
-        <div className="mb-[48px] md:hidden"></div>
+        )}
       </div>
+      <div className="mb-[58px] md:hidden"></div>
     </>
   );
 };
@@ -88,7 +108,7 @@ const NavbarItem = ({ name, to }: NavbarItemProps) => {
         <li
           className={clsx(
             pathname == to &&
-              "text-white md:text-primary font-medium underline underline-offset-4",
+              "text-white md:text-primary font-light md:font-medium underline underline-offset-4",
             "hover:text-white md:hover:text-primary"
           )}
         >
