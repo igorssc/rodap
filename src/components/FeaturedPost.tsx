e2cd4861189caaa29dd7d146c44013d5397593a4
Igor Santos <igorsantoscosta@gmail.com>
2023-01-28 12:28:24 -0300

    Edit "FeaturedPost" component responsive from iOS #7

--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -10,17 +10,15 @@ interface FeaturedPostProps {
 export const FeaturedPost = ({ isSummary = false }: FeaturedPostProps) => {
   return (
     <>
-      <div className="bg-purple-200 h-full">
-        <Image
-          src="https://picsum.photos/400/300"
-          width={400}
-          height={300}
-          alt="Imagem de destaque do Post"
-          className={clsx(
-            "object-center rounded min-w-full min-h-full object-cover"
-          )}
-        />
-      </div>
+      <Image
+        src="https://picsum.photos/400/300"
+        width={400}
+        height={300}
+        alt="Imagem de destaque do Post"
+        className={clsx(
+          "object-center rounded min-w-full min-h-full object-cover"
+        )}
+      />
       <div
         className={clsx(
           "flex flex-col md:gap-0 justify-between summary",
